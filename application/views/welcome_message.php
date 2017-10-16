051a410b59a335ccd7f66c3d16f9768f054c3d86
Juan Eduardo Perez Olea <perezoleaj@gmail.com>
2017-10-16 17:32:03 -0300

    nuevo commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -72,10 +72,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<div id="body">
 		<p>Juan</p>
-		<p>Juan</p>
-		<p>Juan</p>
-		<p>Juan</p>
-		
+		<p>AGREGO NUEVO PARRAFO</p>
+		<p>Otro Cambio</p>
+			
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
